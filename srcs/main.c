39db4cde3f28a3b29196270fef6c50662d75f4f3
winash1618 <asharafbarcalona786@gmail.com>
2023-07-09 10:15:32 +0000

    [so_long : adding textures, maps, made the mlx_loop work]

--- srcs/main.c
+++ srcs/main.c
@@ -1,26 +1,30 @@
 #include "so_long.h"
 
-// int	ft_init_game(t_game *game, char *map_path)
-// {
-// 	if (ft_parse_map(game, map_path) == -1)
-// 		return (-1);
-// 	return (0);
-// }
+int	ft_init_game(t_game *game, char *map_path)
+{
+	// if (ft_parse_map(game, map_path) == -1)
+	// 	return (-1);
+	// return (0);
+  (void) map_path;
+  (void) game;
+  return (1);
+}
 
 int main(int argc, char **argv)
 {
-  (void) argc, (void) argv;
-	// t_game game;
+  // (void) argc;
+  // (void) argv;
+	t_game game;
 
-	// if (argc != 2)
-	// 	return (0);
+	if (argc != 2)
+		return (0);
 	// if (ft_init_game(&game, argv[1]) == -1)
 	// 	return (0);
 	// char **map = parse_map(argv[1]);
 	// (void)map;
-	// game.mlx = mlx_init();
-	// game.win = mlx_new_window(game.mlx, 1000, 1000, "fdf");
+	game.mlx = mlx_init();
+	game.win = mlx_new_window(game.mlx, 1000, 1000, "so_long");
 
-	// mlx_loop(game.mlx);
+	mlx_loop(game.mlx);
 	return (0);
 }
